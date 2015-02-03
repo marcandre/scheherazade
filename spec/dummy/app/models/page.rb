744cc528ddc1323cfa46015052f9576fecc327b9
Marc-Andre Lafortune <github@marc-andre.ca>
2015-02-03 13:52:58 -0500

    Tweak spec models

--- spec/dummy/app/models/page.rb
+++ spec/dummy/app/models/page.rb
@@ -1,6 +1,6 @@
 class Page < ActiveRecord::Base
   belongs_to :website
-  has_many :sections
+  has_many :sections, inverse_of: :page
 
   validates_presence_of :website
 end
--- spec/dummy/app/models/section.rb
+++ spec/dummy/app/models/section.rb
@@ -1,5 +1,5 @@
 class Section < ActiveRecord::Base
-  belongs_to :page
+  belongs_to :page, inverse_of: :sections
 
   validates_presence_of :header, :page
 end
